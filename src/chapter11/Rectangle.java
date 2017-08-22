f5548102419e4284daf1096af1a2dfd35f53b8e0
Toni Ilieva <tilieva@bg.ibm.com>
2017-08-22 11:17:49 +0300

    Review Chapter 11

--- src/chapter11/Rectangle.java
+++ src/chapter11/Rectangle.java
@@ -2,37 +2,37 @@ package chapter11;
 
 import java.util.Scanner;
 
+//	РќР°РїРёС€РµС‚Рµ РїСЂРѕРіСЂР°РјР°, РєРѕСЏС‚Рѕ РїРѕ РґР°РґРµРЅРё РґРІР° РєР°С‚РµС‚Р° РЅР°РјРёСЂР° С…РёРїРѕС‚РµРЅСѓР·Р°С‚Р°
+//	РЅР° РїСЂР°РІРѕСЉРіСЉР»РµРЅ С‚СЂРёСЉРіСЉР»РЅРёРє. Р РµР°Р»РёР·РёСЂР°Р№С‚Рµ РІСЉРІРµР¶РґР°РЅРµ РЅР° РґСЉР»Р¶РёРЅРёС‚Рµ РЅР°
+//	РєР°С‚РµС‚РёС‚Рµ РѕС‚ СЃС‚Р°РЅРґР°СЂС‚РЅРёСЏ РІС…РѕРґ, Р° Р·Р° РїСЂРµСЃРјСЏС‚Р°РЅРµС‚Рѕ РЅР° С…РёРїРѕС‚РµРЅСѓР·Р°С‚Р°
+//	РёР·РїРѕР»Р·РІР°Р№С‚Рµ РјРµС‚РѕРґРё РЅР° РєР»Р°СЃР° Math.
 public class Rectangle {
-	private static double cathetus(double a, double b){
-		double sum = Math.pow(a, 2) + Math.pow(b, 2);
-		return sum;
-	}
-	
-	private static double hypothenuse(double a, double b){
-		double hypothenuse = Math.sqrt(cathetus(a, b));
-		return hypothenuse;
-	}
-	
-//	Напишете програма, която по дадени два катета намира хипотенузата
-//	на правоъгълен триъгълник. Реализирайте въвеждане на дължините на
-//	катетите от стандартния вход, а за пресмятането на хипотенузата
-//	използвайте методи на класа Math.
-	
-	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
-		double a, b;
-		do{
-			System.out.println("a = ");
-			a = scan.nextDouble();
-		}while(a <= 0);
-		
-		do{
-			System.out.println("b = ");
-			b = scan.nextDouble();
-		}while(b <= 0);
-		
-		scan.close();
-		
-		System.out.printf("c = %fcm", hypothenuse(a, b));
-	}
+    private static double cathetus(double a, double b) {
+        double sum = Math.pow(a, 2) + Math.pow(b, 2);
+        return sum;
+    }
+
+    private static double hypothenuse(double a, double b) {
+        //you can do it like this, without initializing new variable
+        return Math.sqrt(cathetus(a, b));
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a, b;
+        do {
+            System.out.println("a = ");
+            a = scan.nextDouble();
+        } while (a <= 0);
+
+        //Again this is something you use multiple times and can be extracted in methods
+        do {
+            System.out.println("b = ");
+            b = scan.nextDouble();
+        } while (b <= 0);
+
+        scan.close();
+
+        System.out.printf("c = %fcm", hypothenuse(a, b));
+    }
 }
